f259f7ad6ca99519d3f51dd26cdb0fe54e0fb2e9
Markus Willems <mail@markus-willems.de>
2018-10-17 13:16:29 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+    const div = createElement('div');
+    setText(div, 'Hello, world!');
+    appendElement(document.getElementById('root'), div);
+};
+
+function createElement(type = 'div', options = {}) {
+    const el = document.createElement(type);
+    if (options && typeof options === 'object') {
+        Object.keys(options).forEach(key => {
+            el.setAttribute(key, options[key]);
+        });
+    }
+    return el;
+}
+
+function appendElement(parent, child) {
+    parent.appendChild(child);
+}
+
+function setText(el, text = '') {
+    el.textContent = text;
+}
